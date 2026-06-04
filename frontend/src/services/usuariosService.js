260e3d9242e5023fdcd8f47c0086db55098a7662
Rodrigo Andrade <rodrigo.andrade@udla.edu.ec>
2026-06-04 17:45:45 -0500

    Uso de variable de entorno para API_URL

--- frontend/src/services/usuariosService.js
+++ frontend/src/services/usuariosService.js
@@ -1,6 +1,6 @@
 import { supabase } from "./supabase";
 
-const API_URL = "http://localhost:3000/api/users";
+const API_URL = import.meta.env.VITE_API_URL + "/users";
 
 // =========================
 // CRUD DE USUARIOS (Vía Backend API)
